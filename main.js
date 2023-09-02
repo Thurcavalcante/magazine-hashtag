dca78997e56e0fd76201d5caaa4baa47bd6235ab
Thurcavalcante <arthurcavalcantethur@gmail.com>
2023-09-02 00:16:30 -0300

    JS: Função de abrir e fechar o carrinho de compras e CSS: Estilização do layout com Tailwind CSS

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
 const catalogo = [
   {
     id: 1,
@@ -81,19 +83,21 @@ const catalogo = [
   },
 ];
 
-for(const produtoCatalogo of catalogo){
+for (const produtoCatalogo of catalogo){
     const cartaoProduto = 
-        `<div id="card-produto-1">
+        `<div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
             <img 
                 src="./assets/img/${produtoCatalogo.imagem}" 
                 alt="Produto 1 do Magazine Hashtag"
                 style="height: 300px;"
             />
-            <p>${produtoCatalogo.marca}</p>
+            <p class='marca'>${produtoCatalogo.marca}</p>
             <p>${produtoCatalogo.nome}</p>
             <p>R$: ${produtoCatalogo.preco},00</p>
             <button>Adicionar</button>
         </div>`;
     
-    document.getElementById("container-produto").innerHTML += cartaoProduto;
-}
+        document.getElementById("container-produto").innerHTML += cartaoProduto;
+      }
+      
+      inicializarCarrinho();
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"], // O Tailwind, olha todos os arquivos que for HTML e JS que estiver na pasta base do projeto, Independente do nome dele.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
